e3030908dd0334b0b9d9baa2d4922243b4459ae2
fwar34 <fwar34@126.com>
2020-07-03 18:37:17 +0800

    update

--- goroutine_test1.go
+++ goroutine_test1.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
+	//"runtime"
 )
 
 func say(s string) {
-    for i := 0; i < 5; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Println(s)
-    }
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		//runtime.Gosched()
+		fmt.Println(s)
+	}
 }
 
 func main() {
-    go say("world")
-    say("hello")
+	go say("world")
+	say("hello")
 }
